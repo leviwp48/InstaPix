70645b5e6816bc3316b7793d5b2378da4160835a
leviwp48 <leviwp48@gmail.com>
2018-12-16 22:57:52 -0800

    Finished some required user stories. Working on style of app

--- app/src/main/java/com/example/levi_pc/instapix/LoginActivity.java
+++ app/src/main/java/com/example/levi_pc/instapix/LoginActivity.java
@@ -3,6 +3,7 @@
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,23 +19,46 @@ import com.parse.ParseUser;
      private EditText etUsername;
      private EditText etPassword;
      private Button btnLogin;
+     private Button btnSignUp;
 
      @Override
      protected void onCreate(Bundle savedInstanceState) {
          super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_login);
 
-         etUsername = findViewById(R.id.etUsername);
-         etPassword = findViewById(R.id.etPassword);
-         btnLogin = findViewById(R.id.btnLogin);
-         btnLogin.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 String username = etUsername.getText().toString();
-                 String password = etPassword.getText().toString();
-                 login(username, password);
-             }
-         });
+         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+         setSupportActionBar(toolbar);
+         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+         ParseUser currentUser = ParseUser.getCurrentUser();
+         if(currentUser != null) {
+             goMainActivity();
+         }
+         else {
+             etUsername = findViewById(R.id.etUsername);
+             etPassword = findViewById(R.id.etPassword);
+             btnLogin = findViewById(R.id.btnLogin);
+             btnLogin.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View v) {
+                     String username = etUsername.getText().toString();
+                     String password = etPassword.getText().toString();
+                     login(username, password);
+                 }
+             });
+             btnSignUp = findViewById(R.id.btnSignUp);
+             btnSignUp.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View v) {
+                     signUp();
+                 }
+             });
+         }
+     }
+
+     private void signUp() {
+         Intent i = new Intent(this, SignUpActivity.class);
+         startActivity(i);
      }
 
      private void login(String username, String password) {
---
+++ app/src/main/java/com/example/levi_pc/instapix/SignUpActivity.java
@@ -0,0 +1,61 @@
+package com.example.levi_pc.instapix;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private EditText etEmail;
+    private Button btnCreateAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        etEmail = findViewById(R.id.etEmail);
+        btnCreateAccount = findViewById(R.id.btnCreateAccount);
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                String email = etEmail.getText().toString();
+                signUp(username, password, email);
+            }
+        });
+    }
+
+    private void signUp(String username, String password, String email) {
+        ParseUser user = new ParseUser();
+        // Create the ParseUser
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        // Set custom properties
+        user.put("phone", "650-253-0000");
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    finish();
+                } else {
+                    e.printStackTrace();
+                    return;
+                }
+            }
+        });
+    }
+}
